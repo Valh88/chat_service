814eee1b5e1a9eac15f8cccc4b766b9a31d63b90
Vano <simplmd@gmail.com>
2025-02-20 14:59:05 +0700

    presence

--- apps/pub_sub/lib/pub_sub/topic.ex
+++ apps/pub_sub/lib/pub_sub/topic.ex
@@ -1,5 +1,5 @@
 defmodule PubSub.Topic do
-  alias Db.{Contacts, Users}
+  alias Db.{Contacts}
   alias PubSub.Presence
 
   @table PubSub.DispatcherRegistry
@@ -12,6 +12,14 @@ defmodule PubSub.Topic do
   end
 
   def unregister(topic) do
+    fun = fn data ->
+      for {pid, id} <- data do
+        if topic != id do
+          send(pid, {:broadcast, {:status, topic, "offline"}})
+        end
+      end
+    end
+    Registry.dispatch(@table, topic, fun)
     Registry.unregister(@table, topic)
   end
 
@@ -21,22 +29,20 @@ defmodule PubSub.Topic do
 
   def subscripe_another_user_if_in_contacts(user_id) do
     users_on_sub_to_user = Contacts.contact_users_by_contact_id(user_id)
+
+    fun = fn data ->
+      for {pid, id} <- data do
+        if user_id != id do
+          send(pid, {:broadcast, {:status, user_id, "online"}})
+        end
+      end
+    end
     for user <- users_on_sub_to_user do
       if Presence.is_online?(user) do
         register(user_id, user)
+        Registry.dispatch(@table, user_id, fun)
       else
-        # IO.inspect(contact)
-      end
-    end
-  end
-
-  def subscribe_to_contacts(user_id) do
-    user = Users.get_by_id(user_id, :preload)
-    for contact <- user.contacts  do
-      if Presence.is_online?(contact.id) do
-        register(contact.id, user_id)
-      else
-        # IO.inspect(contact)
+        # IO.inspect("dsadsad")
       end
     end
   end
--- apps/server/lib/server/client_socket.ex
+++ apps/server/lib/server/client_socket.ex
@@ -2,15 +2,16 @@ defmodule Server.ClientSocket do
   @behaviour WebSock
 
   alias Plug.Session
+  alias Plug.Session
   alias PubSub.{Topic, Presence}
   alias Server.Session
+  alias Db.Users
 
   def init([options]) do
     user = Session.get_session(options)
     Presence.change_status_online(user.id)
     Topic.subscripe_another_user_if_in_contacts(user.id)
-    Topic.subscribe_to_contacts(user.id)
-    send(self(), :push)
+    send(self(), {:event, :contacts})
     {:ok, options}
   end
 
@@ -20,22 +21,42 @@ defmodule Server.ClientSocket do
   end
 
   def handle_in({"Hello, WebSocket!", [opcode: :text]}, state) do
-    # {:reply, :ok, {:text, "pong"}, state}
     {:reply, :ok, {:text, "pong"}, state}
   end
 
-  def terminate(:timeout, state) do
-    IO.inspect("termin")
-    {:ok, state}
-  end
+  # def terminate(:timeout, state) do
+  #   IO.inspect("termin")
+  #   {:ok, state}
+  # end
 
   def handle_info(:push, state) do
     {:reply, :ok, {:text, "test"}, state}
   end
 
+  def handle_info({:broadcast, {:status, user_id, status}}, state) do
+    json = Jason.encode!(%{status: %{user:  %{id: user_id,  status: status}}})
+    {:push, {:text, json}, state}
+  end
+
+  def handle_info({:event, :contacts}, state) do
+    user = Session.get_session(state)
+    user = Users.get_by_id(user.id, :preload)
+    fun = fn contact ->
+      if Presence.is_online?(contact.id) do
+        Topic.register(contact.id, user.id)
+        %{user: contact.id, username: contact.username, status: "online"}
+      else
+        %{user: contact.id, username: contact.username, status: "offline"}
+      end
+    end
+    contacts = Enum.map(user.contacts, fun)
+    json = Jason.encode!(%{contacts: contacts})
+    {:reply, :ok, {:text, json}, state}
+  end
+
   def terminate(_mess, state) do
     user = Session.get_session(state)
-    Session.delete_session(state)
+    # Session.delete_session(state)
     Presence.change_status_offline(user.id)
     Topic.unregister(user.id)
     {:stop, :normal, state}
